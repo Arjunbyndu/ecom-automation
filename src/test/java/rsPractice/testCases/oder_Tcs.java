c7a7f8d2c6f91299244ffea39de5070161f60653
Arjun <arjunbyndoorkar@gmail.com>
2025-05-14 19:05:21 +0530

    Added place order testcase

--- src/test/java/rsPractice/testCases/oder_Tcs.java
+++ src/test/java/rsPractice/testCases/oder_Tcs.java
@@ -1,5 +1,31 @@
 package rsPractice.testCases;
 
-public class oder_Tcs {
+import java.time.Duration;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class oder_Tcs 
+{
+
+	@Test
+	public  void testcase2() throws InterruptedException
+	{
+	
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		
+		
+		driver.get("https://rahulshettyacademy.com/practice-project");
+		driver.findElement(By.xpath("//input[@placeholder='Your Name*']")).sendKeys("Arjun");
+		driver.findElement(By.xpath("//input[@placeholder='Your Email*']")).sendKeys("arjunbyndu@gmail.com");
+		driver.findElement(By.xpath("//input[@type='checkbox']")).click();
+		driver.findElement(By.cssSelector("#form-submit:nth-of-type(1)")).click();
+	}
 }
